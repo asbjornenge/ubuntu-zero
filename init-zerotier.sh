17fe03b7fe8e64bddeda3fa152b166ba1b57ed62
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-09-02 20:25:39 +0200

    Improved output

--- init-zerotier.sh
+++ init-zerotier.sh
@@ -17,37 +17,39 @@ fi
 
 ## Init ZeroTier
 #
-echo '*** ZeroTier Node Ready: '"$ZEROTIER_MEMBER_NAME"
+echo -n '*** ZeroTier Node: '"$ZEROTIER_MEMBER_NAME"
 chown -R daemon /var/lib/zerotier-one
 chgrp -R daemon /var/lib/zerotier-one
 su daemon -s /bin/bash -c '/zerotier-sdk-service -d -U -p9993 >>/tmp/zerotier-sdk-service.out 2>&1'
 virtip4=""
 ZEROTIER_ADDRESS=""
-
-# Wait for ZEROTIER_ADDRESS
 while [ -z "$ZEROTIER_ADDRESS" ]; do
 	sleep 0.2
 	ZEROTIER_ADDRESS=$(zerotier-cli info | awk '{print $3}')
 done
+echo " OK"
 
-# Joing the ZEROTIER_NETWORK_ID
-echo '*** ZeroTier Connect Network: '"$ZEROTIER_NETWORK_ID"
+## Join Network
+# 
+echo -n '*** ZeroTier Connect Network: '"$ZEROTIER_NETWORK_ID"
 zerotier-cli join $ZEROTIER_NETWORK_ID > /dev/null
 sleep 0.2
-
 # Register with ZeroTier API
 function zregister {
   curl -s -X POST --header "Content-Type: application/json" --header "Accept: application/json" --header 'Authorization: Bearer '$ZEROTIER_API_TOKEN https://my.zerotier.com/api/network/$1/member/$ZEROTIER_ADDRESS -d '{"name":"'$ZEROTIER_MEMBER_NAME'","config":{"authorized":true}}' > /dev/null
 }
 zregister $ZEROTIER_NETWORK_ID
+echo " OK"
 
-# Wait for IP
+## Wait for IP
+#
+echo -n '*** ZeroTier Address: '
 while [ -z "$virtip4" ]; do
 	sleep 0.2
 	virtip4=`/zerotier-cli listnetworks | grep -F $ZEROTIER_NETWORK_ID | cut -d ' ' -f 9 | sed 's/,/\n/g' | grep -F '.' | cut -d / -f 1`
 	dev=`/zerotier-cli listnetworks | grep -F "" | cut -d ' ' -f 8 | cut -d "_" -f 2 | sed "s/^<dev>//" | tr '\n' '\0'`
 done
-echo '*** ZeroTier Address: '$virtip4
+echo "$virtip4 OK"
 
 # --- Test section ---
 sleep 0.5
